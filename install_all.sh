3e01c5060c272e05f170585e23241f8734614d50
Ankit Srivastava <asrivast@gatech.edu>
2014-11-05 21:50:35 -0500

    Some corrections. Note to self: don't copy blindly.

--- install_all.sh
+++ install_all.sh
@@ -2,14 +2,14 @@
 
 #installs vim (including vundle and ycm)
 
-# 1.) copy the vimrc
-cp ~/.vim/vimrc.cp ~/.vimrc
+# 1.) create the vimrc
+$HOME/.vim/create_vimrc.sh
 
 # 2.) install vundle
-~/.vim/install_vundle.sh
+$HOME/.vim/install_vundle.sh
 
 # 3.) install all bundles by starting vim
 vim +BundleInstall +qall
 
 # 4.) install (and compile) YCM
-~/.vim/install_ycm.sh
+$HOME/.vim/install_ycm.sh
